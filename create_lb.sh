456f44e32660bb96ea6a8fc6b121a30b66dd9de6
prasadperla55 <137373532+prasadperla55@users.noreply.github.com>
2024-08-08 17:12:05 +0530

    Delete create_lb.sh

--- create_lb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo -e "Setting up load balancer for below IPs\n$(cat IPs.txt)"
-for i in `cat IPs.txt`
-do
-	echo "Adding $i to Load Balancer.."
-done
-	echo "LB set up done! Use 'www.flipkart-qa.com' to access the environment"
-
